9d72190951c2cbbf987c781a67b436a762dc6ad4
discpic-ron <granddriod1210@gmail.com>
2026-01-19 18:20:58 -0500

    Update vasm.py

--- source/vasm.py
+++ source/vasm.py
@@ -28,3 +28,10 @@ def jump(vm,index):
 def out(vm):
   print(f"[DISPLAY]: {vm.registers['A']}")
   vm.arrow += 1
+  
+def check(vm,target_value, jump_location):
+  if str(vm.registers["A"]) == str(target_value):
+    vm.arrow = int(jump_location)
+    print(f"  [Logic] Match found! Jumping to {jump_location}")
+  else:
+    vm.arrow += 1
