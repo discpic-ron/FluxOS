7ac9420945a145368d4cbec884df365afc1567b0
discpic-ron <granddriod1210@gmail.com>
2026-01-19 19:47:25 -0500

    Update kernel.py

--- source/structure/kernel.py
+++ source/structure/kernel.py
@@ -4,6 +4,9 @@ def load_OS(vm, instruction_list, start_at=0):
   for i, instruction in enumerate(instruction_list):
     vm.ram[i + start_at] = instruction
   print(f"KERNEL: Loaded {len(instruction_list)} instructions starting at Locker {start_at}")
-
-instruction_list = []
+  
+instruction_list = [
+   "SET A FluXOS_v1.0_2026",
+   "PRINT",
+]
 load_OS(vm, instruction_list)
